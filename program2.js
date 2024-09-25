870812b99c93ab6f7e8cdfbaf803b3900dd7d207
vaishnavi-pansari <pansarivaishnavi@gmail.com>
2024-09-25 13:32:45 +0530

    Add files via upload

--- program2.js
+++ program2.js
@@ -1,7 +1,30 @@
 const decodeTheRing = function (s, p) {
+        let i = 0; // message index
+        let j = 0; // pattern index
+      
+        while (i < s.length && j < p.length) {
+          if (pattern[j] === '*') {
+            // star symbol, skip to next pattern character
+            j++;
+          } else if (p[j] === '?') {
+            // question mark, match any single letter
+            i++;
+            j++;
+          } else if (s[i] === p[j]) {
+            // exact match, move to next characters
+            i++;
+            j++;
+          } else {
+            // no match, return false
+            return false;
+          }
+        }
+      
+        // check if we've reached the end of both strings
+        return i === s.length && j === p.length;
+      }
 
-    // write your code here
-
-  };
   
-  module.exports = decodeTheRing;
+  
+ 
+
